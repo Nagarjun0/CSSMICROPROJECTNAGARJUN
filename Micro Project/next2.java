4c54914af8c36257ebd9a257079b78dd5b5fba67
Nagarjun Bhole <94926976+Nagarjun0@users.noreply.github.com>
2021-12-19 18:59:16 +0530

    Add files via upload

---
+++ Micro Project/next2.java
@@ -0,0 +1,19 @@
+<html>
+<head>
+<body style="background-color: gold">
+</body>
+<center>
+<h1> Welcome to Quize Compitetion</h1>
+<div>
+<h3>Q.2) What is CSS</h3>
+<input type="radio" name="gender" value="male" > male <br>
+<input type="radio" name="gender" value="male" > male <br>
+<input type="radio" name="gender" value="male" > male <br>
+<input type="radio" name="gender" value="male" > male <br>
+</div>
+<a href="next2.html">
+<button>NEXT</button>
+</center>
+</head>
+</body>
+</html>
